0fb6b10de4e422026b5a78d617cf4308badb5e0f
[Blaine Prickett] <[blaineprickett@gmail.com]>
2023-06-27 15:11:55 -0500

    finished main method

--- ChristmasWithTypes/Program.cs
+++ ChristmasWithTypes/Program.cs
@@ -1,34 +1,32 @@
 ﻿using System;
+using ChristmasWithTypes;
 
-namespace ChristmasWithTypes
+internal class Program
 {
-    class Program
+    private static void Main(string[] args)
     {
-        static void Main(string[] args)
+        var xmas = new Christmas();
+
+        var xmasDay = Christmas.Day.Thursday;
+
+        //DONE set Santa's name to Kris Kringle
+        xmas.Santa = "Kris Kringle";
+
+        //DONE Insert 3 presents you would like for xmas.  They must be strings
+        xmas.Presents = new string[3] { "XBox", "Nintendo Switch", "iPhone X" };
+
+        //DONE Set the TreeHeight to 10
+        xmas.TreeHeight = 10;
+
+        Console.WriteLine($"This year Christmas falls on {xmasDay} \n");
+        Console.WriteLine($"Our tree will be {xmas.TreeHeight} feet high \n");
+        Console.WriteLine("Here are the presents we would like:");
+
+        foreach (var present in xmas.Presents)
         {
-            var xmas = new Christmas();
-
-            var xmasDay = Christmas.Day.Thursday;
-
-            //TODO set Santa's name to Kris Kringle
-            xmas.Santa = "Kris Kringle";
-
-            //TODO Insert 3 presents you would like for xmas.  They must be strings
-            xmas.Presents =  new string[3]{ "XBox", "Playstation", "Cat" }; 
-
-            //TODO Set the TreeHeight to 10
-            xmas.TreeHeight = 10;
-
-            Console.WriteLine($"This year christmas falls on {xmasDay} \n");
-            Console.WriteLine($"Our tree will be {xmas.TreeHeight} feet high \n");
-            Console.WriteLine("Here are the presents we would like:");
-
-            foreach (var present in xmas.Presents)
-            {
-                Console.WriteLine($"    {present}");
-            }
-
-            Console.WriteLine($"\n We like to call Santa, {xmas.Santa}");
+            Console.WriteLine($"    {present}");
         }
+
+        Console.WriteLine($"\n We like to call Santa, {xmas.Santa}");
     }
 }
